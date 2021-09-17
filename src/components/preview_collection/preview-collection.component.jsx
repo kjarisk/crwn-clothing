b107936dd275aec55b9dbb2ca898904b42bd7b0c
Kjartan Kristjansson <kjarisk@gmail.com>
2021-09-17 12:24:25 +0200

    Update shopdata, added cartutils for grouping

--- src/components/preview_collection/preview-collection.component.jsx
+++ src/components/preview_collection/preview-collection.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
       .filter((item, idx) => idx < 4)
-      .map(({id, ...otherItemProps}) => (
-        <CollectionItem key={id} {...otherItemProps} />
+      .map((itm) => (
+        <CollectionItem key={itm.id} item={itm} />
       ))}
     </div>
   </div>
